6cfacf86c91bf5a4d39dee86c904aac6ba25b3ef
prashantmishra-ai <thisisprashantmishra@gmail.com>
2022-08-06 22:52:23 +0530

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = ()=>{
+    return (
+        <>
+        <strong>This is the Footer which is another Component</strong>
+        </>
+    )
+}  
+
+export default Footer;
